7781b4a89615bbf79fb5f5b4886b3ddb964e6326
johnvincentio <john@johnvincent.io>
2021-09-03 16:31:19 -0400

    add

---
+++ udemy-gatsby-shopify/Archive/project-1/Archive/components/Layout/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutWrapper from './styles';
+
+const Layout = ({ children }) => (
+	<>
+		<LayoutWrapper>
+			<main>{children}</main>
+		</LayoutWrapper>
+	</>
+);
+
+export default Layout;
